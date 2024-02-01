b05c44cfbf7a5d559762f5b3de47db649c23f7b3
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-02-01 12:20:08 +0100

    create Review

---
+++ backend/src/main/java/com/bonifert/backend/model/Review.java
@@ -0,0 +1,23 @@
+package com.bonifert.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+  @Id
+  @GeneratedValue
+  private Long id;
+  @CreationTimestamp
+  private LocalDateTime time;
+}
