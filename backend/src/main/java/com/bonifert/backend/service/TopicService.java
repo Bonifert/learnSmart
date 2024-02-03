681d9e0d6371b9d2b547dcaecf9bab0ed7bb15e8
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-02-03 12:25:58 +0100

    create basic TopicService

---
+++ backend/src/main/java/com/bonifert/backend/service/TopicService.java
@@ -0,0 +1,48 @@
+package com.bonifert.backend.service;
+
+import com.bonifert.backend.dto.topic.NewTopicDTO;
+import com.bonifert.backend.dto.topic.TopicDTO;
+import com.bonifert.backend.model.Term;
+import com.bonifert.backend.model.Topic;
+import com.bonifert.backend.model.UserEntity;
+import com.bonifert.backend.service.repository.TopicRepository;
+import com.bonifert.backend.service.repository.UserEntityRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class TopicService {
+  private final TopicRepository topicRepository;
+  private final UserEntityRepository userEntityRepository;
+
+  public TopicService(TopicRepository topicRepository, UserEntityRepository userEntityRepository) {
+    this.topicRepository = topicRepository;
+    this.userEntityRepository = userEntityRepository;
+  }
+
+  public long create(NewTopicDTO newTopicDTO) { // how to authenticate
+    UserEntity user = userEntityRepository.findById(newTopicDTO.userId())
+                                          .orElseThrow(() -> new RuntimeException("TODO"));
+    Topic topic = new Topic();
+    topic.setName(newTopicDTO.name());
+    topic.setUserEntity(user);
+    return topicRepository.save(topic).getId();
+  }
+
+  public TopicDTO getByIdWithFilteredTerms(long topicId) {
+    Topic topic = topicRepository.findById(topicId).orElseThrow(() -> new RuntimeException("TODO"));
+    LocalDateTime now = LocalDateTime.now();
+    List<Term> currentTerms = topic.getTerms()
+                                   .stream()
+                                   .filter(term -> term.getNextShowDateTime().isAfter(now))
+                                   .toList();
+    return new TopicDTO(topic.getName(), topicId, topic.getCreatedAt(), topic.getModifiedAt(), currentTerms);
+  }
+
+  public TopicDTO getById(long topicId) {
+    Topic topic = topicRepository.findById(topicId).orElseThrow(() -> new RuntimeException("TODO"));
+    return new TopicDTO(topic.getName(), topicId, topic.getCreatedAt(), topic.getModifiedAt(), topic.getTerms());
+  }
+}
