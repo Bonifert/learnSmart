e88605d2d6065dbf6039c6f90cdc45ef93a0ee40
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-02-03 20:35:10 +0100

    use termDTO

--- backend/src/main/java/com/bonifert/backend/dto/topic/TopicDTO.java
+++ backend/src/main/java/com/bonifert/backend/dto/topic/TopicDTO.java
@@ -1,9 +1,9 @@
 package com.bonifert.backend.dto.topic;
 
-import com.bonifert.backend.model.Term;
+import com.bonifert.backend.dto.term.TermDTO;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record TopicDTO(String name, long id, LocalDateTime createdAt, LocalDateTime modifiedAt, List<Term> terms) {
+public record TopicDTO(String name, long id, LocalDateTime createdAt, LocalDateTime modifiedAt, List<TermDTO> terms) {
 }
