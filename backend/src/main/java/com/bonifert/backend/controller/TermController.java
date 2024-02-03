1bf0296d03e345773300aac916c70930614d8d72
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-02-03 12:27:01 +0100

    create TermController

---
+++ backend/src/main/java/com/bonifert/backend/controller/TermController.java
@@ -0,0 +1,30 @@
+package com.bonifert.backend.controller;
+
+import com.bonifert.backend.dto.term.NewTermDTO;
+import com.bonifert.backend.service.TermService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/term")
+public class TermController {
+  private final TermService termService;
+
+  public TermController(TermService termService) {
+    this.termService = termService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Void> create(NewTermDTO newTermDTO) {
+    long id = termService.create(newTermDTO);
+    return ResponseEntity.created(URI.create(String.format("/api/term/%d", id))).build();
+  }
+
+  @PutMapping("/review/{termId}")
+  public ResponseEntity<Void> createReview(@PathVariable long termId) {
+    termService.createReviewByTermId(termId);
+    return ResponseEntity.ok().build();
+  }
+}
