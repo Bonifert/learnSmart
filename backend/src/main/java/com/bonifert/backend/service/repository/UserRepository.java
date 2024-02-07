07e24771ab09f6b6445412ca88060aba06e7532b
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-02-07 11:35:07 +0100

    create UserRepository

---
+++ backend/src/main/java/com/bonifert/backend/service/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.bonifert.backend.service.repository;
+
+import com.bonifert.backend.model.user.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+  Optional<UserEntity> findByName(String name);
+}
