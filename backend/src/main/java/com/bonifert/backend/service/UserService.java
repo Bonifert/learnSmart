0f0ead2e025f8d152060cd7ac57f391b625e7dd1
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-02-07 11:35:52 +0100

    create UserService

--- backend/src/main/java/com/bonifert/backend/service/UserService.java
+++ backend/src/main/java/com/bonifert/backend/service/UserService.java
@@ -1,21 +1,43 @@
 package com.bonifert.backend.service;
 
 import com.bonifert.backend.dto.user.NewUserDTO;
-import com.bonifert.backend.model.UserEntity;
-import com.bonifert.backend.service.repository.UserEntityRepository;
+import com.bonifert.backend.exception.NotFoundException;
+import com.bonifert.backend.model.user.Role;
+import com.bonifert.backend.model.user.UserEntity;
+import com.bonifert.backend.service.repository.RoleRepository;
+import com.bonifert.backend.service.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-  private final UserEntityRepository userEntityRepository;
+  private final UserRepository userRepository;
+  private final RoleRepository roleRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  public UserService(UserEntityRepository userEntityRepository) {
-    this.userEntityRepository = userEntityRepository;
+  public UserService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.roleRepository = roleRepository;
+    this.passwordEncoder = passwordEncoder;
   }
 
   public long create(NewUserDTO newUserDTO) {
     UserEntity userEntity = new UserEntity();
     userEntity.setName(newUserDTO.name());
-    return userEntityRepository.save(userEntity).getId();
+    userEntity.setPassword(passwordEncoder.encode(newUserDTO.password()));
+    return userRepository.save(userEntity).getId();
   }
+
+  @Transactional
+  public void addRoleToUser(long userId) {
+    UserEntity user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("TODO"));
+    Role role = roleRepository.findByName("ROLE_USER").orElseThrow(() -> new NotFoundException("TODO"));
+    user.addRole(role);
+    userRepository.save(user);
+  }
+
 }
