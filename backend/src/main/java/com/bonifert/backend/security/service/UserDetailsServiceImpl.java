f83f29925ceefe994aae6b57b46a6c5998a75bbc
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-02-07 11:37:02 +0100

    create UserDetailsServiceImpl

---
+++ backend/src/main/java/com/bonifert/backend/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.bonifert.backend.security.service;
+
+import com.bonifert.backend.exception.NotFoundException;
+import com.bonifert.backend.model.user.UserEntity;
+import com.bonifert.backend.service.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  private final UserRepository userRepository;
+
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    UserEntity user = userRepository.findByName(username)
+                                    .orElseThrow(() -> new NotFoundException("User not found with name: " + username));
+    List<? extends GrantedAuthority> grantedAuthorities = user.getRoles()
+                                                              .stream()
+                                                              .map(role -> new SimpleGrantedAuthority(role.getName()))
+                                                              .toList();
+    return new User(username, user.getPassword(), grantedAuthorities);
+  }
+}
