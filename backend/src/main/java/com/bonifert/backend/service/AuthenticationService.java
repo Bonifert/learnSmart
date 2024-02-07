7e94c278cbf56ac709c996d2b494d61dbd869bd4
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-02-07 13:27:36 +0100

    refactor login method, create getUser method

--- backend/src/main/java/com/bonifert/backend/service/AuthenticationService.java
+++ backend/src/main/java/com/bonifert/backend/service/AuthenticationService.java
@@ -1,30 +1,44 @@
 package com.bonifert.backend.service;
 
 import com.bonifert.backend.dto.user.LoginUserDTO;
+import com.bonifert.backend.exception.NotFoundException;
+import com.bonifert.backend.model.user.UserEntity;
 import com.bonifert.backend.security.jwt.JwtUtils;
+import com.bonifert.backend.service.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import org.springframework.http.HttpHeaders;
+
 @Service
 public class AuthenticationService {
   private final AuthenticationManager authenticationManager;
   private final JwtUtils jwtUtils;
+  private final UserRepository userRepository;
 
-  public AuthenticationService(AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
+  public AuthenticationService(AuthenticationManager authenticationManager, JwtUtils jwtUtils,
+                               UserRepository userRepository) {
     this.authenticationManager = authenticationManager;
     this.jwtUtils = jwtUtils;
+    this.userRepository = userRepository;
   }
 
-  public String login(LoginUserDTO loginUserDTO) {
+  public HttpHeaders login(LoginUserDTO loginUserDTO) {
     Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
             loginUserDTO.name(),
             loginUserDTO.password()));
     SecurityContextHolder.getContext().setAuthentication(authentication);
-    return jwtUtils.generateJwtToken(authentication); // is it good? I give the user instead of authentication
+    String token = jwtUtils.generateJwtToken(authentication);
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Authorization", "Bearer " + token);
+    return headers;
   }
 
-  // TODO: getUser method from SecurityContextHolder
+  public UserEntity getUser(){
+    String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+    return userRepository.findByName(userName).orElseThrow(()-> new NotFoundException("User not found"));
+  }
 }
