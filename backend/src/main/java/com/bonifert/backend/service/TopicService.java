4f8292bdc69e82b8b2e6efc210f914e6534fad79
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-02-07 15:18:54 +0100

    refactor TopicService, create methods to edit and delete topics. also create helper methods

--- backend/src/main/java/com/bonifert/backend/service/TopicService.java
+++ backend/src/main/java/com/bonifert/backend/service/TopicService.java
@@ -1,6 +1,7 @@
 package com.bonifert.backend.service;
 
 import com.bonifert.backend.dto.term.TermDTO;
+import com.bonifert.backend.dto.topic.EditTopicDTO;
 import com.bonifert.backend.dto.topic.NewTopicDTO;
 import com.bonifert.backend.dto.topic.TopicDTO;
 import com.bonifert.backend.exception.NotFoundException;
@@ -9,6 +10,7 @@ import com.bonifert.backend.model.Topic;
 import com.bonifert.backend.model.user.UserEntity;
 import com.bonifert.backend.service.repository.TopicRepository;
 import com.bonifert.backend.service.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -29,8 +31,7 @@ public class TopicService {
 
   public long create(NewTopicDTO newTopicDTO) {
     String userName = SecurityContextHolder.getContext().getAuthentication().getName();
-    UserEntity user = userRepository.findByName(userName)
-                                    .orElseThrow(() -> new NotFoundException("User not found"));
+    UserEntity user = userRepository.findByName(userName).orElseThrow(() -> new NotFoundException("User not found"));
     Topic topic = new Topic();
     topic.setName(newTopicDTO.name());
     topic.setUserEntity(user);
@@ -45,24 +46,47 @@ public class TopicService {
                                    .stream()
                                    .filter(term -> term.getNextShowDateTime().isBefore(now))
                                    .toList();
-    return new TopicDTO(topic.getName(),
-                        topicId,
-                        topic.getCreatedAt(),
-                        topic.getModifiedAt(),
-                        convertTermsToDTOs(currentTerms));
+    return convertTopicToDTOWithFilteredTerms(topic, currentTerms);
+  }
+
+  public TopicDTO getById(long topicId) {
+    Topic topic = topicRepository.findById(topicId).orElseThrow(() -> new NotFoundException("Topic not found"));
+    validator.validateTopic(topic);
+    return convertTopicToDTO(topic);
+  }
+
+  public void deleteById(long id) {
+    Topic topic = topicRepository.findById(id).orElseThrow(() -> new NotFoundException("Topic not found"));
+    validator.validateTopic(topic);
+    topicRepository.delete(topic);
+  }
+
+  @Transactional
+  public TopicDTO edit(EditTopicDTO editTopicDTO) {
+    Topic topic = topicRepository.findById(editTopicDTO.topicId())
+                                 .orElseThrow(() -> new NotFoundException("Topic not found"));
+    validator.validateTopic(topic);
+    topic.setName(editTopicDTO.newName());
+    return convertTopicToDTO(topicRepository.save(topic));
   }
 
   private List<TermDTO> convertTermsToDTOs(List<Term> terms) {
     return terms.stream().map(term -> new TermDTO(term.getId(), term.getName(), term.getDefinition())).toList();
   }
 
-  public TopicDTO getById(long topicId) {
-    Topic topic = topicRepository.findById(topicId).orElseThrow(() -> new NotFoundException("Topic not found"));
-    validator.validateTopic(topic);
+  private TopicDTO convertTopicToDTO(Topic topic) {
     return new TopicDTO(topic.getName(),
-                        topicId,
+                        topic.getId(),
                         topic.getCreatedAt(),
                         topic.getModifiedAt(),
                         convertTermsToDTOs(topic.getTerms()));
+  }
+
+  private TopicDTO convertTopicToDTOWithFilteredTerms(Topic topic, List<Term> terms) {
+    return new TopicDTO(topic.getName(),
+                        topic.getId(),
+                        topic.getCreatedAt(),
+                        topic.getModifiedAt(),
+                        convertTermsToDTOs(terms));
   }
 }
