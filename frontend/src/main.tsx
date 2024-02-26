3989115b0c4c4852f701808703978b62de9cd5f1
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-02-26 20:11:25 +0100

    use contexts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import UserProvider from "./context/userContext/UserProvider.tsx";
+import FeedbackContext from "./context/alertContext/FeedbackContext.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <UserProvider>
+        <FeedbackContext>
+          <App/>
+        </FeedbackContext>
+      </UserProvider>
+    </React.StrictMode>,
 )
