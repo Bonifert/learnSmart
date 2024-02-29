f2f3c5b7770de18fb73c1c24b3e7dccb62904277
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-02-29 15:17:34 +0100

    create a method to get the topics by user

--- backend/src/main/java/com/bonifert/backend/service/repository/TopicRepository.java
+++ backend/src/main/java/com/bonifert/backend/service/repository/TopicRepository.java
@@ -1,9 +1,13 @@
 package com.bonifert.backend.service.repository;
 
 import com.bonifert.backend.model.Topic;
+import com.bonifert.backend.model.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+  List<Topic> getAllByUserEntity(UserEntity user);
 }
