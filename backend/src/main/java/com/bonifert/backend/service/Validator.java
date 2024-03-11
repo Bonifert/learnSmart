f2d316556a3a8ebe6e34f93a8cb3d72fa84a3f0a
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-03-12 00:05:52 +0100

    rename method and overload it

--- backend/src/main/java/com/bonifert/backend/service/Validator.java
+++ backend/src/main/java/com/bonifert/backend/service/Validator.java
@@ -1,6 +1,7 @@
 package com.bonifert.backend.service;
 
 import com.bonifert.backend.exception.AccessDeniedException;
+import com.bonifert.backend.model.Term;
 import com.bonifert.backend.model.Topic;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,18 @@ import java.util.Objects;
 
 @Service
 public class Validator {
-  public void validateTopic(Topic topic){
+  public void validate(Topic topic){
     String userName = SecurityContextHolder.getContext().getAuthentication().getName();
     String topicUserName = topic.getUserEntity().getUserName();
     if (!Objects.equals(userName, topicUserName)){
+      throw new AccessDeniedException("Resource permission denied");
+    }
+  }
+
+  public void validate(Term term){
+    String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+    String termUserName = term.getTopic().getUserEntity().getUserName();
+    if (!Objects.equals(userName, termUserName)){
       throw new AccessDeniedException("Resource permission denied");
     }
   }
