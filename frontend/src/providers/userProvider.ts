c68d982294a1b1105053417600210a4c761b90bf
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-03-11 22:26:13 +0100

    remove try catch and make body optional in ApiResObj

--- frontend/src/providers/userProvider.ts
+++ frontend/src/providers/userProvider.ts
@@ -2,24 +2,18 @@ import {UserNamePassword} from "../context/userContext/UserProvider.tsx";
 
 export interface ApiResObj {
   readonly status: number,
-  readonly body: object | null,
-  readonly headers: Headers
+  readonly body?: object
 }
 
 async function login(userInfo: UserNamePassword): Promise<ApiResObj> {
-  try {
-    const httpRes: Response = await fetch("/api/user/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(userInfo)
-    });
-    return {status: httpRes.status, body: null, headers: httpRes.headers};
-  } catch (e) {
-    console.error(e);
-    throw new Error("Unexpected error occurred.");
-  }
+  const httpRes: Response = await fetch("/api/user/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(userInfo)
+  });
+  return {status: httpRes.status};
 }
 
 async function register(userInfo: UserNamePassword): Promise<ApiResObj> {
@@ -30,7 +24,7 @@ async function register(userInfo: UserNamePassword): Promise<ApiResObj> {
     },
     body: JSON.stringify(userInfo),
   });
-  return {status: httpRes.status, body: null, headers: httpRes.headers};
+  return {status: httpRes.status};
 }
 
 export {login, register};
