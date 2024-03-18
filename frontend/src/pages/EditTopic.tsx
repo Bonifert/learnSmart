c0b0e1056e4e476968d1e2d227e41cf60dcb523f
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-03-18 14:04:44 +0100

    recreate EditTopic.tsx (somehow I couldn't ctrl-z the component)

--- frontend/src/pages/EditTopic.tsx
+++ frontend/src/pages/EditTopic.tsx
@@ -1,46 +1,18 @@
 import {useNavigate, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {deleteTopic, getTopicById} from "../providers/topicProvider.ts";
-import TopicForm, {Topic} from "../components/TopicForm.tsx";
+import TopicForm from "../components/TopicForm.tsx";
 import CircularProgress from '@mui/material/CircularProgress';
 import {Box} from "@mui/material";
 import {useFeedback} from "../context/alertContext/feedbackContextImport.ts";
+import {useFetchTopic} from "../hooks/useFetchTopic.tsx";
 
 const EditTopic = () => {
   const {id} = useParams();
   const navigate = useNavigate();
   const {feedback} = useFeedback();
-  const [topic, setTopic] = useState<Topic | null>(null);
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    async function getTopic() {
-      setLoading(true);
-      try {
-        if (id) {
-          const response = await getTopicById(parseInt(id));
-          if (response.status === 200) {
-            setTopic(response.body as Topic)
-          } else if( response.status === 404){
-            feedback("The topic doesn't exist.", "error");
-            navigate("/");
-          } else if(response.status === 401){
-            feedback("You don't have access to this resource.", "error");
-          }
-        } else {
-          navigate("/");
-        }
-      } catch (e) {
-        console.log(e);
-        feedback("Unexpected error occurred.", "error");
-      } finally {
-        setLoading(false);
-      }
-    }
-    getTopic();
-  }, [id]);
+  const {topic, loading} = useFetchTopic(id, navigate, feedback);
+  console.log(topic);
 
-
-  if (loading) {
+  if (loading || !topic) {
     return (
         <Box height="40vh" sx={{display: 'flex', alignItems: "center", justifyContent: "center"}}>
           <CircularProgress/>
@@ -48,8 +20,8 @@ const EditTopic = () => {
     )
   }
 
-  if (topic) return (
-      <TopicForm topic={topic} disabled={loading} onDelete={deleteTopic}/>
+  return (
+      <TopicForm topic={topic} disabled={loading} />
   );
 };
 
