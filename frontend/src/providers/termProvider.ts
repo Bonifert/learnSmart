d8e86f37ae7e2a1dd744c0f3737e85151a02aa7f
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-03-18 14:05:08 +0100

    create helper method and deleteTerm fetch

--- frontend/src/providers/termProvider.ts
+++ frontend/src/providers/termProvider.ts
@@ -1,5 +1,6 @@
 import {ApiResObj} from "./userProvider.ts";
 import {Term} from "../components/TopicForm.tsx";
+import {getToken} from "./topicProvider.ts";
 
 export interface NewTermDTO {
   topicId: number;
@@ -8,7 +9,7 @@ export interface NewTermDTO {
 }
 
 async function createTerm(newTermDTO: NewTermDTO) : Promise<ApiResObj>{
-  const token = localStorage.getItem("token") ?? "";
+  const token = getToken();
   const httpRes : Response = await fetch("/api/term", {
     method: "POST",
     headers: {
@@ -17,11 +18,12 @@ async function createTerm(newTermDTO: NewTermDTO) : Promise<ApiResObj>{
     },
     body: JSON.stringify(newTermDTO),
   });
-  return {status: httpRes.status};
+  const body = await httpRes.json();
+  return {status: httpRes.status, body};
 }
 
 async function editTerm(editTermDTO: Term) : Promise<ApiResObj>{
-  const token = localStorage.getItem("token") ?? "";
+  const token = getToken();
   const httpRes : Response = await fetch("/api/term", {
     method: "PATCH",
     headers: {
@@ -33,4 +35,15 @@ async function editTerm(editTermDTO: Term) : Promise<ApiResObj>{
   return {status: httpRes.status};
 }
 
-export {createTerm, editTerm};
+async function deleteTerm(id: number) : Promise<ApiResObj>{
+  const token = getToken();
+  const httpRes : Response = await fetch(`/api/term/${id}`,{
+    method: 'DELETE',
+    headers: {
+      "Authorization": token,
+    }
+  });
+  return {status: httpRes.status};
+}
+
+export {createTerm, editTerm, deleteTerm};
