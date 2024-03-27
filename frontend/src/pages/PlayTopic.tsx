52ebaf99ac651a9a61323ec60e72a704a7c18c0e
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-03-27 12:21:04 +0100

    create components that fetch the topic, one with filtered terms and another with all terms

---
+++ frontend/src/pages/PlayTopic.tsx
@@ -0,0 +1,25 @@
+import {useNavigate, useParams} from "react-router-dom";
+import {useFeedback} from "../context/alertContext/feedbackContextImport.ts";
+import {TopicType, useFetchTopic} from "../hooks/useFetchTopic.tsx";
+import {Box} from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+import TopicGame from "../components/TopicGame.tsx";
+
+const PlayTopic = () => {
+  const {id} = useParams();
+  const {feedback} = useFeedback();
+  const navigate = useNavigate();
+  const {topic, loading, reloadTopic} = useFetchTopic(id, navigate, feedback, TopicType.ALL);
+
+  if (loading || !topic) {
+    return (
+        <Box height="40vh" sx={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+          <CircularProgress/>
+        </Box>
+    );
+  }
+
+  return <TopicGame topic={topic} onReloadTopic={reloadTopic}/>;
+};
+
+export default PlayTopic;
