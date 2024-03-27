5102059fdf565106cf96e5bfa04dff547b24a516
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-03-27 12:30:28 +0100

    create TopicUrgencyService

---
+++ backend/src/main/java/com/bonifert/backend/service/TopicUrgencyService.java
@@ -0,0 +1,28 @@
+package com.bonifert.backend.service;
+
+import com.bonifert.backend.model.Term;
+import com.bonifert.backend.model.Topic;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class TopicUrgencyService {
+  public String calculateUrgency(Topic topic){
+    int expiredCounter = 0;
+    List<Term> terms = topic.getTerms();
+    for (Term term : terms){
+      if (term.getNextShowDateTime().isBefore(LocalDateTime.now())) {
+        expiredCounter++;
+      }
+    }
+    if (expiredCounter == 0) {
+      return "Optional";
+    }
+    if ((expiredCounter / (double) terms.size()) * 100 < 30) {
+      return "Consider";
+    }
+    return "Prioritize";
+  }
+}
