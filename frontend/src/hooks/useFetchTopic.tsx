4fbb5e580888f7d8f4b30c1cf5e8e69c63e1b827
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-03-27 12:14:03 +0100

    make useFetchTopic able to fetch topic with filtered terms, and also add a reloadTopic function

--- frontend/src/hooks/useFetchTopic.tsx
+++ frontend/src/hooks/useFetchTopic.tsx
@@ -1,45 +1,59 @@
 import {useEffect, useState} from "react";
 import {Topic} from "../components/TopicForm.tsx";
-import {getTopicById} from "../providers/topicProvider.ts";
+import {getFilteredTopicById, getTopicById} from "../providers/topicProvider.ts";
 import {Variant} from "../context/alertContext/FeedbackContext.tsx";
 
-const useFetchTopic = (id: string | undefined, navigate: (path: string)=> void, feedback: (message: string, type: Variant)=> void ) => {
+export enum TopicType {
+  ALL,
+  FILTERED
+}
+
+const useFetchTopic = (id: string | undefined, navigate: (path: string)=> void, feedback: (message: string, type: Variant)=> void, topicType: TopicType) => {
   const [topic, setTopic] = useState<Topic | null>(null);
   const [loading, setLoading] = useState(false);
 
   if (!id){
     navigate("/");
   }
+  async function fetchTopic() {
+    setLoading(true);
+    try {
+      if (id) {
+        let response;
+        if (topicType === TopicType.ALL){
+          response = await getTopicById(id);
+        } else {
+          response = await getFilteredTopicById(id);
+        }
+        if (response.status === 200) {
+          setTopic(response.body as Topic);
+        } else if (response.status === 404) {
+          feedback("The topic doesn't exist.", "error");
+          navigate("/");
+        } else if (response.status === 403) {
+          feedback("You don't have access to this resource.", "error");
+          navigate("/")
+        }
+      } else {
+        navigate("/");
+      }
+    } catch (e) {
+      console.log(e);
+      feedback("Unexpected error occurred.", "error");
+    } finally {
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
-    async function fetchTopic() {
-      setLoading(true);
-      try {
-        if (id) {
-          const response = await getTopicById(id);
-          if (response.status === 200) {
-            setTopic(response.body as Topic);
-          } else if (response.status === 404) {
-            feedback("The topic doesn't exist.", "error");
-            navigate("/");
-          } else if (response.status === 403) {
-            feedback("You don't have access to this resource.", "error");
-            navigate("/")
-          }
-        } else {
-          navigate("/");
-        }
-      } catch (e) {
-        console.log(e);
-        feedback("Unexpected error occurred.", "error");
-      } finally {
-        setLoading(false);
-      }
-    }
     fetchTopic();
   }, [id]);
 
-  return { topic, loading };
+  function reloadTopic(){
+    fetchTopic();
+  }
+
+  return { topic, loading, reloadTopic };
 };
 
 export {useFetchTopic};
