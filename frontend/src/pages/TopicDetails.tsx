889201d2e3a733e395c30a2fe80745c76302628b
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-03-27 12:26:29 +0100

    refactor component

--- frontend/src/pages/TopicDetails.tsx
+++ frontend/src/pages/TopicDetails.tsx
@@ -1,54 +1,114 @@
 import {useNavigate, useParams} from "react-router-dom";
 import {useFeedback} from "../context/alertContext/feedbackContextImport.ts";
-import {useFetchTopic} from "../hooks/useFetchTopic.tsx";
+import {TopicType, useFetchTopic} from "../hooks/useFetchTopic.tsx";
 import CircularProgress from "@mui/material/CircularProgress";
-import {Box, Grid, Stack} from "@mui/material";
+import {Accordion, AccordionDetails, AccordionSummary, Box, Grid, Stack} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+const buttonSx = {color: "white", bgcolor: "#469ca3", borderColor: "#469ca3", "&:hover": {bgcolor: "#18838c"}, boxShadow: 2};
+const centerSx = {display: "flex", alignItems: "center", justifyContent: "center"};
 
 const TopicDetails = () => {
   const {id} = useParams();
   const navigate = useNavigate();
   const {feedback} = useFeedback();
-  const {topic, loading} = useFetchTopic(id, navigate, feedback);
+  const {topic, loading} = useFetchTopic(id, navigate, feedback, TopicType.ALL);
 
   if (loading || !topic) {
     return (
-        <Box height="40vh" sx={{display: 'flex', alignItems: "center", justifyContent: "center"}}>
+        <Box height="40vh" sx={centerSx}>
           <CircularProgress/>
         </Box>
     );
   }
 
+  function urgencyCalculator(givenDate: Date): string {
+    const now = new Date();
+    if (givenDate < now) {
+      return "Pending";
+    } else {
+      return "Fulfilled";
+    }
+  }
+
+  function formatDate(givenDate: Date) {
+    const today = new Date();
+
+    if (givenDate.getDate() === today.getDate() &&
+        givenDate.getMonth() === today.getMonth() &&
+        givenDate.getFullYear() === today.getFullYear()) {
+      return givenDate.toLocaleTimeString('en-US', {hour: 'numeric', minute: '2-digit', hour12: true});
+    } else {
+      return givenDate.toLocaleDateString('en-US', {month: 'short', day: 'numeric', year: 'numeric'});
+    }
+  }
+
   return (
-      <Box height="100%" width="100%" sx={{bgcolor: "#d1e6e8", justifyContent: 'center', alignItems: 'center'}}>
+      <Box height="100%" width="100%" sx={{bgcolor: "#E1F7FAFF", ...centerSx}}>
         <Grid my={4} container
-              sx={{bgcolor: "#d1e6e8", display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <Grid item xs={10} sm={8} md={7} lg={6}  minWidth="275px"
-                sx={{bgcolor: "#A3CDD1", borderRadius: 2}}>
+              sx={{bgcolor: "#E1F7FAFF", ...centerSx}}>
+          <Grid item xs={10} sm={8} md={7} lg={6} minWidth="275px"
+                sx={{bgcolor: "#A3CDD1", borderRadius: 2, boxShadow: 3}}>
             <Grid container>
               <Grid item xs={8}>
                 <Box m={3}>
                   <Typography variant="h4">
                     {topic.name}
                   </Typography>
+                  <Box my={4} color={"#676767"}>
+                    <Typography>
+                      Created at: {formatDate(new Date(topic.createdAt))}
+                    </Typography>
+                    <Typography>
+                      Modified at: {formatDate(new Date(topic.modifiedAt))}
+                    </Typography>
+                  </Box>
+                  <Typography>Priority: {topic.priority}</Typography>
                 </Box>
               </Grid>
               <Grid item xs={4} sx={{display: "flex", alignItems: "center", justifyContent: "flex-end"}}>
-                <Stack spacing={2} m={3}>
-                  <Button sx={{color: "black", bgcolor: "#469ca3", borderColor: "#469ca3"}}
+                <Stack spacing={1} m={3}>
+                  <Button sx={buttonSx}
                           variant="outlined"
                           color="inherit"
-                          onClick={()=> navigate(`/edit/${topic!.id}`)}>Edit topic</Button>
-                  <Button sx={{color: "black", bgcolor: "#469ca3", borderColor: "#469ca3"}}
+                          onClick={() => navigate(`/edit/${topic!.id}`)}>Edit topic</Button>
+                  <Button sx={buttonSx}
                           variant="outlined"
-                          color="inherit">Play all</Button>
-                  <Button sx={{color: "black", bgcolor: "#469ca3", borderColor: "#469ca3"}}
+                          color="inherit"
+                          onClick={() => navigate(`/play/${topic?.id}/all`)}>Play all</Button>
+                  <Button sx={buttonSx}
                           variant="outlined"
-                          color="inherit">Play filter</Button>
+                          color="inherit"
+                          onClick={() => navigate(`/play/${topic?.id}/filtered`)}>Play pending</Button>
                 </Stack>
               </Grid>
             </Grid>
+            <Box m={3}>
+              <Typography variant="h5">Terms:</Typography>
+              <Grid container>
+                {topic.terms.map(term => (
+                    <Grid item xs={12} sm={6}>
+                      <Accordion sx={{m: 1}}>
+                        <AccordionSummary
+                            expandIcon={<ArrowDownwardIcon/>}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                          <Typography sx={{width: '65%', flexShrink: 0}}>{term.name}</Typography>
+                          <Typography
+                              sx={{color: 'text.secondary'}}>{urgencyCalculator(new Date(term.nextShowDateTime))}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <Typography>
+                            {term.definition}
+                          </Typography>
+                        </AccordionDetails>
+                      </Accordion>
+                    </Grid>
+                ))}
+              </Grid>
+            </Box>
           </Grid>
         </Grid>
       </Box>
