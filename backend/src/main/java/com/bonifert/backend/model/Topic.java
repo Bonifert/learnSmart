7c6ac842af4fad90f6206387e0156d0067d2f45b
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-04-10 22:20:38 +0200

    set default name

--- backend/src/main/java/com/bonifert/backend/model/Topic.java
+++ backend/src/main/java/com/bonifert/backend/model/Topic.java
@@ -3,6 +3,7 @@ package com.bonifert.backend.model;
 import com.bonifert.backend.model.user.UserEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -19,7 +20,7 @@ public class Topic {
   @Id
   @GeneratedValue
   private Long id;
-  private String name;
+  private String name = "";
   @ManyToOne
   private UserEntity userEntity;
   @CreationTimestamp
