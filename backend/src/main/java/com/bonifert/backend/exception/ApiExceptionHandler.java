47efc87a03d283267dc05b29da79803896540823
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-04-12 14:06:20 +0200

    add method to controller advice to catch the exceptions from validation

--- backend/src/main/java/com/bonifert/backend/exception/ApiExceptionHandler.java
+++ backend/src/main/java/com/bonifert/backend/exception/ApiExceptionHandler.java
@@ -4,8 +4,16 @@ import com.google.gson.JsonSyntaxException;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
@@ -14,6 +22,34 @@ public class ApiExceptionHandler {
     HttpStatus httpStatus = HttpStatus.NOT_FOUND;
     ErrorResponse errorResponse = new ErrorResponse(exception.getMessage(), httpStatus);
     return new ResponseEntity<>(errorResponse, httpStatus);
+  }
+
+  @ExceptionHandler(value = MethodArgumentNotValidException.class)
+  public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+    HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+    Map<String, String> errors = new HashMap<>();
+
+    exception.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    return new ResponseEntity<>(errors, httpStatus);
+  }
+
+  @ExceptionHandler(value = MethodArgumentTypeMismatchException.class) // todo idk why but spring never throw this exception when can't create an enum
+  public ResponseEntity<Object> handleTypeMismatchExceptions(MethodArgumentTypeMismatchException ex) {
+    Map<String, Object> response = new HashMap<>();
+    Class<?> requiredType = ex.getRequiredType();
+    if (requiredType != null && requiredType.isEnum()) {
+      response.put("errors", "Invalid value for " + ex.getName() + ". Please provide one of the following values: " +
+              Arrays.stream(requiredType.getEnumConstants())
+                    .map(Object::toString)
+                    .collect(Collectors.joining(", ")));
+    } else {
+      response.put("errors", "Invalid value for " + ex.getName() + ". Please provide a valid value.");
+    }
+    return ResponseEntity.badRequest().body(response);
   }
 
   @ExceptionHandler(value = OpenAIException.class)
