8a804069272f085ff0fcb264b62dba2bca447a9e
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-04-18 10:24:41 +0200

    change max userName and password character length to 35

--- backend/src/main/java/com/bonifert/backend/dto/user/NewUserDTO.java
+++ backend/src/main/java/com/bonifert/backend/dto/user/NewUserDTO.java
@@ -9,11 +9,11 @@ public record NewUserDTO(
         @Valid
         @NotBlank(message = "userName is mandatory")
         @NotNull(message = "userName is mandatory")
-        @Size(min = 5, max = 15, message = "The userName must be min 5 and max 15 character.")
+        @Size(min = 5, max = 35, message = "The userName must be min 5 and max 35 character.")
         String userName,
 
         @NotBlank(message = "password is mandatory")
         @NotNull(message = "password is mandatory")
-        @Size(min = 5, max = 15, message = "The password must be min 5 and max 15 character.")
+        @Size(min = 5, max = 35, message = "The password must be min 5 and max 35 character.")
         String password) {
 }
