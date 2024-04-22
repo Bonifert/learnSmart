8796dff4db184d29739abdde0c49d9c60834221d
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-04-22 15:33:53 +0200

    create RegistrationFormSchema

---
+++ frontend/src/components/types/RegistrationFormSchema.ts
@@ -0,0 +1,23 @@
+import {z, ZodType} from "zod";
+import {RegistrationData} from "./RegistrationData.ts";
+
+export const RegistrationFormSchema: ZodType<RegistrationData> = z
+    .object({
+      username: z
+          .string({required_error: "Username is required"})
+          .trim()
+          .min(1, {message: "Username is required"})
+          .min(5, {message: "Minimum 5 character"})
+          .max(35, {message: "Maximum 35 character"}),
+      password: z
+          .string({required_error: "Password is required"})
+          .trim()
+          .min(1, {message: "Password is required"})
+          .min(5, {message: "Minimum 5 character"})
+          .max(35, {message: "Maximum 35 character"}),
+      confirmPassword: z.string()
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    });
