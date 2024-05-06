55e303c3d09c68265ec94da4e77347a9487c7d6d
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-05-06 20:55:44 +0200

    change register endpoint url, and remove login endpoint

--- backend/src/main/java/com/bonifert/backend/controller/UserController.java
+++ backend/src/main/java/com/bonifert/backend/controller/UserController.java
@@ -1,12 +1,9 @@
 package com.bonifert.backend.controller;
 
-import com.bonifert.backend.dto.user.LoginUserDTO;
 import com.bonifert.backend.dto.user.NewUserDTO;
 import com.bonifert.backend.dto.user.UserInformationDTO;
-import com.bonifert.backend.service.AuthenticationService;
 import com.bonifert.backend.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -17,27 +14,19 @@ import java.net.URI;
 @RequestMapping("/api/user")
 public class UserController {
   private final UserService userService;
-  private final AuthenticationService authenticationService;
 
-  public UserController(UserService userService, AuthenticationService authenticationService) {
+  public UserController(UserService userService) {
     this.userService = userService;
-    this.authenticationService = authenticationService;
   }
 
-  @PostMapping
+  @PostMapping("/create")
   public ResponseEntity<Void> register(@Valid @RequestBody NewUserDTO newUserDTO) {
-    long id = userService.register(newUserDTO);
+    long id = userService.createUser(newUserDTO);
     return ResponseEntity.created(URI.create(String.format("/api/user/%d", id))).build();
   }
 
-  @PostMapping("/login")
-  public ResponseEntity<Void> login(@RequestBody LoginUserDTO loginUserDTO) {
-    HttpHeaders jwt = authenticationService.login(loginUserDTO);
-    return ResponseEntity.ok().headers(jwt).build();
-  }
-
   @GetMapping("/me")
-  public ResponseEntity<UserInformationDTO> me(){
+  public ResponseEntity<UserInformationDTO> me() {
     String username = SecurityContextHolder.getContext().getAuthentication().getName();
     return ResponseEntity.ok().body(new UserInformationDTO(username));
   }
