dee05285040f946f327b288f841370d9a0e8c4c6
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-05-06 19:34:12 +0200

    add TopicService as dependency, and use the getTopicById and refreshUrgencyAndSave method

--- backend/src/main/java/com/bonifert/backend/service/TermService.java
+++ backend/src/main/java/com/bonifert/backend/service/TermService.java
@@ -9,7 +9,6 @@ import com.bonifert.backend.model.Topic;
 import com.bonifert.backend.service.mapper.TermMapper;
 import com.bonifert.backend.service.repository.ReviewRepository;
 import com.bonifert.backend.service.repository.TermRepository;
-import com.bonifert.backend.service.repository.TopicRepository;
 import com.bonifert.backend.service.showCalculator.NextShowCalculator;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -19,29 +18,26 @@ import java.time.LocalDateTime;
 @Service
 public class TermService {
   private final TermRepository termRepository;
-  private final TopicRepository topicRepository;
   private final ReviewRepository reviewRepository;
   private final NextShowCalculator nextShowCalculator;
   private final Validator validator;
   private final TermMapper termMapper;
-  private final TopicUrgencyService topicUrgencyService;
+  private final TopicService topicService;
 
-  public TermService(TermRepository termRepository, TopicRepository topicRepository, ReviewRepository reviewRepository,
+  public TermService(TermRepository termRepository, ReviewRepository reviewRepository,
                      NextShowCalculator nextShowCalculator, Validator validator, TermMapper termMapper,
-                     TopicUrgencyService topicUrgencyService) {
+                     TopicService topicService) {
     this.termRepository = termRepository;
-    this.topicRepository = topicRepository;
     this.reviewRepository = reviewRepository;
     this.nextShowCalculator = nextShowCalculator;
     this.validator = validator;
     this.termMapper = termMapper;
-    this.topicUrgencyService = topicUrgencyService;
+    this.topicService = topicService;
   }
 
   @Transactional
   public TermDTO create(NewTermDTO newTermDTO) {
-    Topic topic = topicRepository.findById(newTermDTO.topicId())
-                                 .orElseThrow(() -> new NotFoundException("Topic not found"));
+    Topic topic = topicService.getTopicById(newTermDTO.topicId());
     validator.validate(topic);
     Term term = new Term();
     term.setName(newTermDTO.name());
@@ -49,13 +45,12 @@ public class TermService {
     term.setTopic(topic);
     term.setNextShowDateTime(LocalDateTime.now());
     topic.addTerm(term);
-    topic.setPriority(topicUrgencyService.calculateUrgency(topic));
-    topicRepository.save(topic);
+    topicService.refreshUrgencyAndSave(topic);
     return termMapper.toTermDTO(termRepository.save(term));
   }
 
-  public void delete(long id){
-    Term term = termRepository.findById(id).orElseThrow(()-> new NotFoundException("Term not found"));
+  public void delete(long id) {
+    Term term = termRepository.findById(id).orElseThrow(() -> new NotFoundException("Term not found"));
     validator.validate(term);
     termRepository.delete(term);
   }
@@ -67,26 +62,23 @@ public class TermService {
     Review review = new Review();
     reviewRepository.save(review);
     term.addReview(review);
-    if (LocalDateTime.now().isAfter(term.getNextShowDateTime())){
+    if (LocalDateTime.now().isAfter(term.getNextShowDateTime())) {
       LocalDateTime next = calculateNextShowDate(term);
       term.setNextShowDateTime(next);
-      Topic topic = term.getTopic();
-      String topicUrgencyLevel = topicUrgencyService.calculateUrgency(topic);
-      topic.setPriority(topicUrgencyLevel);
-      topicRepository.save(topic);
+      topicService.refreshUrgencyAndSave(term.getTopic());
     }
     termRepository.save(term);
   }
 
   @Transactional
-  public void edit(TermDTO termDTO){
-    Term term = termRepository.findById(termDTO.id()).orElseThrow(()-> new NotFoundException("Term not found"));
+  public void edit(TermDTO termDTO) {
+    Term term = termRepository.findById(termDTO.id()).orElseThrow(() -> new NotFoundException("Term not found"));
     term.setDefinition(termDTO.definition());
     term.setName(termDTO.name());
     termRepository.save(term);
   }
 
-  private LocalDateTime calculateNextShowDate(Term term){
+  private LocalDateTime calculateNextShowDate(Term term) {
     return nextShowCalculator.calculate(term.getReviews());
   }
 }
